f8be3d5b4a9f786af72da57238a8d3e150e55010
Aulas Ingenieria <aulasingenieria@ujaverianas.loc>
2026-04-07 12:27:05 -0500

    feat(toast): implementar setState centralizado Closes #300

--- apps/web/src/components/UI/use-toast.js
+++ apps/web/src/components/UI/use-toast.js
@@ -10,5 +10,14 @@ const toastStore = {
     listeners: [],
 
 	getState: () => toastStore.state,
+	setState: (nextState) => {
+    if (typeof nextState === 'function') {
+        toastStore.state = nextState(toastStore.state)
+    } else {
+        toastStore.state = { ...toastStore.state, ...nextState }
+    }
+
+    toastStore.listeners.forEach(listener => listener(toastStore.state))
+},
 }
 
