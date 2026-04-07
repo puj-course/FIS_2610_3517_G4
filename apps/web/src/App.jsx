d9536ffc6b7b46b1b730f42a6d4605945b2eae24
solonlosada2006 <solon.losada2006@gmail.com>
2026-04-07 12:16:12 -0500

    Implementación en app.jsx sustituyéndolo por el archivo de configuración Closes #279

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,35 +1,32 @@
+// apps/web/src/App.jsx
 import React from 'react';
 import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
 
 // Estructura y Seguridad
 import { AuthProvider } from './contexts/AuthContext';
 import DashboardLayout from './layouts/DashboardLayout';
+import ProtectedRoute from './components/ProtectedRoute'; 
 
-
-import HomePage from './pages/HomePage';
-import TeamPage from './pages/TeamPage';
-
-
-import DashboardPage from './pages/DashboardPage';
-//import ValidacionRUNTPage from "./pages/validacionRunt.jsx";
+import { routesConfig } from './Routes/routesConfig'; 
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          {/* 1. Rutas Públicas */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/team" element={<TeamPage />} />
-
-          {/* 2. Rutas Privadas */}
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
           
-          
+          {routesConfig.map((route) => {
+            
+            return (
+              <Route 
+                key={route.path} 
+                path={route.path} 
+                element={route.element} 
+              />
+            );
+          })}
 
-          {/* 4. Redirección global */}
+          
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
