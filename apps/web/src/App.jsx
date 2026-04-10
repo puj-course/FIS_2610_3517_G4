6c935b40ae118b1deceb14119e946b1924a695e1
Sarm-m <ramirezm.sa@javeriana.edu.co>
2026-04-10 00:12:12 -0500

    Fixes code App.jsx

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,22 +1,12 @@
-// apps/web/src/App.jsx
 import React from 'react';
 import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
-
-// Estructura y Seguridad
-import { AuthProvider } from './contexts/AuthContext';
-import DashboardLayout from './layouts/DashboardLayout';
-import ProtectedRoute from './components/ProtectedRoute'; 
-
-import { routesConfig } from './Routes/routesConfig'; 
-
-=======
 import { AuthProvider } from '@/contexts/AuthContext.jsx';
 import ProtectedRoute from '@/components/ProtectedRoute.jsx';
 import DashboardLayout from '@/layouts/DashboardLayout.jsx';
 
 import HomePage from '@/pages/HomePage.jsx';
 import TeamPage from '@/pages/TeamPage.jsx';
+import LoginPage from '@/pages/LoginPage.jsx';
 
 import DashboardPage from '@/pages/DashboardPage.jsx';
 import AlertasPage from '@/pages/AlertasPage.jsx';
@@ -26,39 +16,16 @@ import DocumentosPage from '@/pages/DocumentosPage.jsx';
 import ValidacionRUNTPage from '@/pages/ValidacionRUNTPage.jsx';
 import ReportesPage from '@/pages/ReportesPage.jsx';
 import ConfiguracionPage from '@/pages/ConfiguracionPage.jsx';
->>>>>>> origin/develop
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-<<<<<<< HEAD
-          
-          {routesConfig.map((route) => {
-            
-            const elementToRender = route.protected ? (
-              <ProtectedRoute>
-                <DashboardLayout>
-                  {route.element}
-                </DashboardLayout>
-              </ProtectedRoute>
-            ) : (
-              route.element
-            );
-
-            return (
-              <Route 
-                key={route.path} 
-                path={route.path} 
-                element={elementToRender} 
-              />
-            );
-          })}
-=======
           {/* Públicas */}
           <Route path="/" element={<HomePage />} />
           <Route path="/team" element={<TeamPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
           {/* Protegidas */}
           <Route
@@ -77,7 +44,6 @@ function App() {
             <Route path="/reportes" element={<ReportesPage />} />
             <Route path="/configuracion" element={<ConfiguracionPage />} />
           </Route>
->>>>>>> origin/develop
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
